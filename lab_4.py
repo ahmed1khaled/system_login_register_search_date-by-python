1777157ab8e4725cd777a54cc4998857535254d9
ahmed khaled sedky <76923062+ahmed1khaled@users.noreply.github.com>
2022-07-19 13:53:25 +0200

    Add files via upload

---
+++ lab_4.py
@@ -0,0 +1,49 @@
+"""
+iti-2022
+Python Course 2022
+By Muhammad Nasser
+"""
+
+# lab4:
+
+'''
+
+Gathered funding console app:
+
+    Gathered funding is the practice of funding a project or venture by raising small
+    amounts of money from a large number of people, typically via the Internet.
+    
+The aim of the project: Create a console app to start fundraise projects.
+
+    The app should include the following features:
+    
+    1 - Authentication System:
+        • Registration:
+            • First name
+            • Last name
+            • Email
+            • Password
+            • Confirm password
+            • Mobile phone [validated against Egyptian phone numbers]
+        • Login
+            • The user should be able to login after registration using his email and password
+        
+    2 - Projects:
+    
+        • The user can create a project fund raise campaign which contains:
+        • Title
+        • Details
+        • Total target (i.e 250000 EGP)
+        • Set start/end time for the campaign (validate the date formula)
+        
+    User can view all projects
+    User can edit his own projects
+    User can delete his own project
+    User can search for a project using date (bonus)
+    
+Similar Projects to get some inspiration :)
+https://www.gofundme.com https://www.kickstarter.com https://www.crowdfunding.com
+
+'''
+
+# write your code here:
